a1362e987af52f75033585856691974c95cc1c95
saxenasanket <saxenasanket135@gmail.com>
2017-12-02 13:19:43 +0530

    Update overall_performance.php

--- graphs/overall_performance.php
+++ graphs/overall_performance.php
@@ -2422,7 +2422,7 @@ error: function(data) {
         <!-- mini logo for sidebar mini 50x50 pixels -->
         <span class="logo-mini"><b>A</b>LT</span>
         <!-- logo for regular state and mobile devices -->
-        <span class="logo-lg"><b>LITTLE FLOWER</b></span>
+        <span class="logo-lg"><b>LITTLE FLOWE</b></span>
       </a>
 
       <!-- Header Navbar: style can be found in header.less -->
